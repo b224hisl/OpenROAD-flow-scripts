6a05c114e5d1ab56dee7f7202fe2087a5fadc963
Yifei Zhu <b224hisl@yandex.com>
2023-03-27 16:08:08 +0000

    usable openroad

--- flow/designs/asap7/bfly/config.mk
+++ flow/designs/asap7/bfly/config.mk
@@ -5,5 +5,5 @@ export DESIGN_NAME            = bfly
 export VERILOG_FILES          = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v))
 export SDC_FILE               = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
-
+export CORE_UTILIZATION       = 5
 export PLACE_DENSITY          = 0.35
--- flow/designs/asap7/gcd/config.mk
+++ flow/designs/asap7/gcd/config.mk
@@ -5,6 +5,8 @@ export DESIGN_NAME            = gcd
 export VERILOG_FILES          = $(sort $(wildcard ./designs/src/$(DESIGN_NICKNAME)/*.v))
 export SDC_FILE               = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
+
+export CORE_UTILIZATION       = 5
 export DIE_AREA               = 0 0 16.2 16.2
 export CORE_AREA              = 1.08 1.08 15.12 15.12
 export PLACE_DENSITY          = 0.35
